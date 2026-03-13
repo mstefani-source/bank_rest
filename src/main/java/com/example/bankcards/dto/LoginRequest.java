ebf27661f2e0391866ea4f49c0b4ef3c2da1e0d7
Victor <devitos76@gmial.com>
2026-03-13 23:27:21 +0300

    refactor: organize files

---
+++ src/main/java/com/example/bankcards/dto/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.example.bankcards.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Schema(description = "Запрос на аутентификацию")
+public class LoginRequest {
+
+    @Schema(description = "логин (e-mail", example = "John")
+    @NotBlank(message = "Email не может быть пустыми")
+    private String email;
+
+
+    @Schema(description = "Пароль", example = "my_1secret1_password")
+    @Size(min = 8, max = 255, message = "Длина пароля должна быть от 8 до 255 символов")
+    @NotBlank(message = "Пароль не может быть пустыми")
+    private String password;
+}
